88c8116376286ae49619e898f2d6b55edac12b04
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-04-22 16:48:59 +0300

    index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import createTheme from '@material-ui/core/styles/createTheme';
+import { ThemeProvider } from '@material-ui/styles';
+import { blue } from '@material-ui/core/colors';
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: blue[900],
+		},
+	},
+});
+
+ReactDOM.render(
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
